14f90dda098dfd3817b3d73c29cd0875121b99bc
BoevMaksim <maksim.schetina@gmail.com>
2019-05-02 13:18:34 +0300

    Budget calculator

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let start = document.querySelector('#start'),
+    value = document.querySelectorAll('.budget-value, .daybudget-value, .level-value, .expenses-value, .optionalexpenses-value, .income-value, .monthsavings-value, .yearsavings-value'),
+    expInput = document.querySelectorAll('.expenses-item'),
+    data = document.querySelector('.data'),
+    btn = data.getElementsByTagName('button'),
+    btnExp = btn[1]
+    btnOptExp = btn[2]
+    btnCount = btn[3]
+    optExp = document.querySelectorAll('.optionalexpenses-item')
+    chooseIncome = document.querySelector('.choose-income-label')
+    checkSavings = document.querySelector('.checksavings')
+    chooseSum = document.querySelector('.choose-sum')
+    choosePercent = document.querySelector('.choose-percent')
+    timeData = document.querySelectorAll('.year, .month, .day');
+
+console.log();
